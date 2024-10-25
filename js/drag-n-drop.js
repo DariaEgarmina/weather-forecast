e26bbccf15360c62ab8e1730de8acda23bc59648
Daria Egarmina <d.egarmina@gmail.com>
2024-10-25 19:03:50 +0300

    Продолжает реализовывать перетаскивание карточек

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -1,5 +1,6 @@
 const weatherContentContainer = document.querySelector('.weather-content__result');
 const smallCards = document.querySelectorAll('.small-card');
+const bigCards = document.querySelectorAll('.big-card');
 
 const makeCardsDraggable = (cardElements) => {
   for (const card of cardElements) {
@@ -7,7 +8,56 @@ const makeCardsDraggable = (cardElements) => {
   }
 };
 
+const getNextElement = (cursorPosition, currentElement) => {
+
+  if (currentElement.classList.contains('big-card__header') ||
+    currentElement.classList.contains('big-card__content')) {
+    currentElement = currentElement.closest('.big-card');
+  }
+
+  const currentElementCoord = currentElement.getBoundingClientRect();
+  const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+  const nextElement = (cursorPosition < currentElementCenter) ?
+    currentElement :
+    currentElement.nextElementSibling;
+
+  return nextElement;
+};
+
+const moveElement = (evt) => {
+  const activeElement = weatherContentContainer.querySelector('.selected');
+
+  const currentElement = evt.target;
+
+  const isMoveable = activeElement !== currentElement &&
+    currentElement.classList.contains('small-card') ||
+    currentElement.classList.contains('big-card__header') ||
+    currentElement.classList.contains('big-card__content');
+
+  if (!isMoveable) {
+    return;
+  }
+
+  const nextElement = getNextElement(evt.clientY, currentElement);
+
+  if (
+    nextElement &&
+    activeElement === nextElement.previousElementSibling ||
+    activeElement === nextElement
+  ) {
+    return;
+  }
+
+  if (nextElement === null) {
+    currentElement.after(activeElement);
+  } else {
+    nextElement.before(activeElement);
+  }
+};
+
 makeCardsDraggable(smallCards);
+makeCardsDraggable(bigCards);
 
 weatherContentContainer.addEventListener('dragstart', (evt) => {
   evt.target.classList.add('selected');
@@ -15,4 +65,9 @@ weatherContentContainer.addEventListener('dragstart', (evt) => {
 
 weatherContentContainer.addEventListener('dragend', (evt) => {
   evt.target.classList.remove('selected');
+});
+
+weatherContentContainer.addEventListener('dragover', (evt) => {
+  evt.preventDefault();
+  moveElement(evt);
 });
