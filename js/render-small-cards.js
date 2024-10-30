473bc1dbf3d3c604b422fd4e729711bb39328b32
Daria Egarmina <d.egarmina@gmail.com>
2024-10-30 14:17:14 +0300

    Дорабатывает создание маленькой карточки, добавляет ветер и погодные условия

--- js/render-small-cards.js
+++ js/render-small-cards.js
@@ -3,11 +3,33 @@ import { cities } from './mock-data.js';
 const smallCardsContainer = document.querySelector('.weather-content__small-cards');
 const smallCardTemplate = document.querySelector('#small-card').content.querySelector('.small-card');
 
+const createWeatherConditions = (weatherObject) => {
+  const fragment = document.createDocumentFragment();
+
+  for (const key in weatherObject) {
+    if (weatherObject[key] === true) {
+      const weatherConditionElement = document.createElement('span');
+      weatherConditionElement.classList.add('icon');
+      weatherConditionElement.classList.add(`icon--${key}`);
+      fragment.append(weatherConditionElement);
+    }
+  }
+
+  return fragment;
+};
+
 const createSmallCardElement = (city) => {
   const smallCardElement = smallCardTemplate.cloneNode(true);
 
   smallCardElement.querySelector('.small-card__city').textContent = city.city;
   smallCardElement.querySelector('.small-card__temperature').textContent = city.temperature;
+
+  smallCardElement.querySelector('.big-card__wind-info').textContent =
+  `Ветер ${city.wind.direction}, ${city.wind.speed} м/с`;
+
+  const weatherConditions = createWeatherConditions(city.weather);
+  smallCardElement.querySelector('.big-card__weather-conditions').innerHTML = '';
+  smallCardElement.querySelector('.big-card__weather-conditions').append(weatherConditions);
 
   return smallCardElement;
 };
