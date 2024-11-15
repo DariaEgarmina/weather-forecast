f2357af3517ec3bd1aed857ff667d344406e69f3
Daria Egarmina <d.egarmina@gmail.com>
2024-11-15 13:00:52 +0300

    Завершает большой рефакторинг

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 import './mocks/cities.js';
 
-import './state/cities.js';
-import './state/favorite-cities.js';
-
 import './components/render-small-cards.js';
-import './components/render-big-cards.js';
-
 import './search.js';
+
+import './components/render-big-cards.js';
 import './sort.js';
 import './filter.js';
+
+import './state/cities.js';
+import './state/favorite-cities.js';
 
 import './create-weather-conditions.js';
 import './create-temperature.js';
